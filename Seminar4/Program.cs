fca5691b9db302b3369dfba69056e508e6c464d5
Julia <wuwara@mail.ru>
2022-12-11 18:54:45 +0300

    Seminar 4

---
+++ Seminar4/Program.cs
@@ -0,0 +1,90 @@
+﻿// Напишите задачу, которая принимает на вход число А и выдаёт сумму чисел от 1 до А
+
+// int SummNumbers(int num){
+//     int summ = 0;
+//     for(int i = 1; i <= num; i++)
+//         summ += i;
+//     return summ;
+// }
+// Console.WriteLine("Enter your number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int summ = SummNumbers(num);
+// Console.WriteLine($"summ of numbers from 1 to {num} is {summ}");
+
+
+
+// int SummNumbers(int num){
+//     int summ = 0;
+//     int i = 1;
+//     while(i <= num){
+//         summ += i;
+//         i++;
+//     }
+//     return summ;
+// }
+// Console.Write("Enter your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int summ = SummNumbers(num);
+// Console.WriteLine($"summ of numbers from 1 to {num} is {summ}");
+
+
+
+
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// int Counter(int num)
+// {   int count = 0;
+//     if (num == 0) count = 1;
+//     while (num > 0)
+//     {
+//         num = num / 10;
+//         count += 1;
+//     }
+//     return count;
+// }
+
+// Console.Write("Enter your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = Counter(num);
+// Console.WriteLine($"Количество цифр в нашем числе: {count}");
+
+
+
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// int Fact (int a){
+//     int sum = 1;
+//     for (int i = 1; i <= a; i++){
+//         sum = sum * i;
+//     }
+//     return sum;
+// }
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int print = Fact(a);
+// System.Console.WriteLine(print);
+
+
+// Написать программу, которая выводит массив из 8ми элементов, заполненный нулями и единицами в случайном порядке
+
+int[] Array()
+{
+    int [] arr = new int [8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+
+    return arr;
+}
+void ShowArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+}
+
+
+ShowArray(Array());
+ 
